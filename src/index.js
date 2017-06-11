1fb84141b2a8efaec63f3fba350508015f75cf18
Sean Rose <sean1rose@gmail.com>
2017-06-10 17:38:19 -0700

    base off react starter pack + add data in json/csv format

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'sweetsweetfantasybaby';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+
+console.log('THIS IS A TEST FOO');
