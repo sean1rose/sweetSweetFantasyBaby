868ffae7f47c79f94548ae8b40808e5db8c67322
Sean Rose <sean1rose@gmail.com>
2017-06-11 01:30:20 -0700

    battle component with child player input field component

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import PlayerInput from './PlayerInput';
 
+// RECAP: https://www.youtube.com/watch?v=z_OpiP_b6HY @ 20:30 
 class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, playername) {
+    this.setState(() => {
+      var newState = {};
+      newState[id + 'Name'] = playername;
+      return newState;
+    });
+  }
+  
   render() {
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
     return (
       <div>
-        BATTLE!
+        <div className='row'>
+          {!playerOneName && 
+          <PlayerInput
+            id='playerOne'
+            label='Player One'
+            onSubmit={this.handleSubmit} />}
+
+          {!playerTwoName &&
+          <PlayerInput
+            id='playerTwo'
+            label='Player Two'
+            onSubmit={this.handleSubmit} />}
+
+        </div>
       </div>
     );
   }
