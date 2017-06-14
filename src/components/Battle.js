e7e55a2ced5e59fd8010688b74ad92a3b4c38607
Sean Rose <sean1rose@gmail.com>
2017-06-14 01:54:54 -0700

    set up display of spider chart with highcharts by parsing stats

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -3,8 +3,15 @@ import PlayerInput from './PlayerInput';
 import {Link} from 'react-router-dom';
 import getRb2016 from '../util/getRb2016';
 import StackedBars from './charts/StackedBars';
+import ReactHighcharts from 'react-highcharts';
+import HighchartsMore from 'highcharts-more';
 
-console.log('getrb - ', getRb2016);
+// HighchartsMore(ReactHighcharts.Highcharts);
+
+
+console.log('getrbOnes - ', getRb2016.getRbOnes());
+console.log('getrbTwos - ', getRb2016.getRbTwos());
+console.log('getrbThrees - ', getRb2016.getRbThrees());
 
 // RECAP: https://www.youtube.com/watch?v=z_OpiP_b6HY @ 20:30 
 class Battle extends Component {
@@ -16,11 +23,79 @@ class Battle extends Component {
       playerTwoName: '',
       playerOneData: {},
       playerTwoData: {},
-      displayChart: false
+      displayChart: false,
+      config: {}
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.calcHigh = this.calcHigh.bind(this);
+    this.calcSecond = this.calcSecond.bind(this);
   }
+
+  /*
+  componentDidUpdate() {
+    // ['FtPts', 'FtPtsGm', 'Touches', 'TouchesGm', 'FtPtsTouch', 'RushYds', 'CatchYds', 'TotalTd']
+    // ['FANTASYPTS', 'FPTSPERGAME', 'TOUCHES', 'TOUCHESPERGAME', 'FANTASYPTSPERTOUCH', 'RYD', 'PYDS', 'TOTALTD']
+    // ftptsHigh ends up being 1, we just need to determine who high is
+    var ftptsHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'FANTASYPTS');
+    // second is proportion of high's value
+    var ftptsSecond = this.calcSecond(ftptsHigh, this.state.playerOneData, this.state.playerTwoData, 'FANTASYPTS');
+    var ftptsgmHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'FPTSPERGAME');
+    var ftptsgmSecond = this.calcSecond(ftptsgmHigh, this.state.playerOneData, this.state.playerTwoData, 'FPTSPERGAME');
+    var touchesHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'TOUCHES');
+    var touchesSecond = this.calcSecond(touchesHigh, this.state.playerOneData, this.state.playerTwoData, 'TOUCHES');
+    var touchesgmHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'TOUCHESPERGAME');
+    var touchesgmSecond = this.calcSecond(touchesgmHigh, this.state.playerOneData, this.state.playerTwoData, 'TOUCHESPERGAME');
+    var ftptstouchHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'FANTASYPTSPERTOUCH');
+    var ftptstouchSecond = this.calcSecond(ftptstouchHigh, this.state.playerOneData, this.state.playerTwoData, 'FANTASYPTSPERTOUCH');
+    var rydHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'RYD');
+    var rydSecond = this.calcSecond(rydHigh, this.state.playerOneData, this.state.playerTwoData, 'RYD');
+    var cydHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'PYDS');
+    var cydSecond = this.calcSecond(cydHigh, this.state.playerOneData, this.state.playerTwoData, 'PYDS');
+    var totalydHigh = this.calcHigh(this.state.playerOneData, this.state.playerTwoData, 'TOTALTD');
+    var totalydSecond = this.calcSecond(totalydHigh, this.state.playerOneData, this.state.playerTwoData, 'TOTALTD');
+
+    this.setState(() => {
+      var newState = {};
+      newState.config = {
+        chart: {
+          polar: true
+        },
+        xAxis: {
+          categories: ['FtPts', 'FtPtsGm', 'Touches', 'TouchesGm', 'FtPtsTouch', 'RushYds', 'CatchYds', 'TotalTd']
+        },
+        series: [{
+          name: this.state.playerOneData.PLAYER,
+          data: [this.state.playerOneData.PLAYER === ftptsHigh ? 1 : ftptsSecond, this.state.playerOneData.PLAYER === ftptsgmHigh ? 1 : ftptsgmSecond, this.state.playerOneData.PLAYER === touchesHigh ? 1 : touchesSecond, this.state.playerOneData.PLAYER === touchesgmHigh ? 1 : touchesgmSecond, this.state.playerOneData.PLAYER === ftptstouchHigh ? 1 : ftptstouchSecond, this.state.playerOneData.PLAYER === rydHigh ? 1 : rydSecond, this.state.playerOneData.PLAYER === cydHigh ? 1 : cydSecond, this.state.playerOneData.PLAYER === totalydHigh ? 1 : totalydSecond],
+          pointPlacement: 'on'
+        }, {
+          name: this.state.playerTwoData.PLAYER,
+          data: [this.state.playerOneData.PLAYER === ftptsHigh ? 1 : ftptsSecond, this.state.playerOneData.PLAYER === ftptsgmHigh ? 1 : ftptsgmSecond, this.state.playerOneData.PLAYER === touchesHigh ? 1 : touchesSecond, this.state.playerOneData.PLAYER === touchesgmHigh ? 1 : touchesgmSecond, this.state.playerOneData.PLAYER === ftptstouchHigh ? 1 : ftptstouchSecond, this.state.playerOneData.PLAYER === rydHigh ? 1 : rydSecond, this.state.playerOneData.PLAYER === cydHigh ? 1 : cydSecond, this.state.playerOneData.PLAYER === totalydHigh ? 1 : totalydSecond],
+          pointPlacement: 'on'
+        }]
+      }
+      return newState;
+    });
+    // var config = {
+    //   chart: {
+    //     polar: true
+    //   },
+    //   xAxis: {
+    //     categories: ['FtPts', 'FtPtsGm', 'Touches', 'TouchesGm', 'FtPtsTouch', 'RushYds', 'CatchYds', 'TotalTd']
+    //   },
+    //   series: [{
+    //     name: this.state.playerOneData.PLAYER,
+    //     data: [this.state.playerOneData.PLAYER === ftptsHigh ? 1 : ftptsSecond, this.state.playerOneData.PLAYER === ftptsgmHigh ? 1 : ftptsgmSecond, this.state.playerOneData.PLAYER === touchesHigh ? 1 : touchesSecond, this.state.playerOneData.PLAYER === touchesgmHigh ? 1 : touchesgmSecond, this.state.playerOneData.PLAYER === ftptstouchHigh ? 1 : ftptstouchSecond, this.state.playerOneData.PLAYER === rydHigh ? 1 : rydSecond, this.state.playerOneData.PLAYER === cydHigh ? 1 : cydSecond, this.state.playerOneData.PLAYER === totalydHigh ? 1 : totalydSecond],
+    //     pointPlacement: 'on'
+    //   }, {
+    //     name: this.state.playerTwoData.PLAYER,
+    //     data: [this.state.playerOneData.PLAYER === ftptsHigh ? 1 : ftptsSecond, this.state.playerOneData.PLAYER === ftptsgmHigh ? 1 : ftptsgmSecond, this.state.playerOneData.PLAYER === touchesHigh ? 1 : touchesSecond, this.state.playerOneData.PLAYER === touchesgmHigh ? 1 : touchesgmSecond, this.state.playerOneData.PLAYER === ftptstouchHigh ? 1 : ftptstouchSecond, this.state.playerOneData.PLAYER === rydHigh ? 1 : rydSecond, this.state.playerOneData.PLAYER === cydHigh ? 1 : cydSecond, this.state.playerOneData.PLAYER === totalydHigh ? 1 : totalydSecond],
+    //     pointPlacement: 'on'
+    //   }]
+    // }
+    
+  }
+  */
 
   handleSubmit(id, playername) {
     // have player name -> want a clean search tho...
@@ -29,8 +104,8 @@ class Battle extends Component {
     var newState = {};
     newState[id + 'Name'] = playername;
 
-    newState[id + 'Data'] = [searchedPlayer[0]];
-    // console.log('newstate - ', newState, id + 'Data' , newState[id + 'Data'], searchedPlayer[0]);
+    newState[id + 'Data'] = [searchedPlayer[0]][0];
+    console.log('newstate - ', newState, id + 'Data' , newState[id + 'Data'], searchedPlayer[0]);
 
     this.setState((prevState, props) => {
       if (prevState.playerOneName || prevState.playerTwoName){
@@ -45,6 +120,14 @@ class Battle extends Component {
       }
     });
   }
+
+  calcHigh(p1Data, p2Data, attribute) {
+    return p1Data[attribute] > p2Data[attribute] ? p1Data.PLAYER : p2Data.PLAYER;
+  }
+
+  calcSecond(high, p1Data, p2Data, attribute) {
+    return high === p1Data.PLAYER ? (p2Data[attribute] / p1Data[attribute]) : (p1Data[attribute] / p2Data[attribute]);
+  }
   
   render() {
     var match = this.props.match;
@@ -52,6 +135,11 @@ class Battle extends Component {
     var playerTwoName = this.state.playerTwoName;
     var playerOneData = this.state.playerOneData;
     var playerTwoData = this.state.playerTwoData;
+
+
+
+    
+
     var displayChart = this.state.displayChart;
     return (
       <div>
@@ -70,8 +158,9 @@ class Battle extends Component {
 
         </div>
 
-        {displayChart && 
-        <StackedBars playerOne={playerOneData} playerTwo={playerTwoData} />}
+        {/*{displayChart && 
+          <ReactHighcharts config={this.state.config} />
+        }*/}
 
       </div>
     );
--- src/util/getRb2016.js
+++ src/util/getRb2016.js
@@ -3,12 +3,55 @@ import Fuse from 'fuse.js';
 // http://fusejs.io/
 
 const getRb2016 = {
-  all: () => { return runningbacks},
+  all: () => { 
+    console.log('get all in util - ', runningbacks);
+    return runningbacks
+  },
   get: (id) => {
     var options = {keys: ['PLAYER'], threshold: 0.3};
     var fuse = new Fuse(runningbacks, options);
     // returns an array w/ results (hopefully only 1 result)
     return fuse.search(id);
+  },
+  calcAdditionalStats: (player) => {
+    // before pushing, attach a few extra stats:
+      // ftpts/touch, total tds, total yds
+    player.FANTASYPTSPERTOUCH = player.FANTASYPTS / (player.CMP + player.CARRIES);
+    player.TOTALTD = player.PTD + player.RTD;
+    player.TOTALYD = player.PYDS + player.RYD;
+    player.FPTSPERGAME = player.FPTS.G;
+    player.TOUCHES = player.CMP + player.CARRIES;
+    player.TOUCHESPERGAME = player.TOUCHES / player.GP;
+    delete player.FPTS;
+    return player;
+  },
+  getRbOnes: () => {
+    // return RB1 avgs (1-12)
+    var result = [];
+    for (var i = runningbacks.length - 1; i >= runningbacks.length - 12; i--){
+      var current = getRb2016.calcAdditionalStats(runningbacks[i]);
+      result.push(current);
+    }
+    return result;
+    // return list;
+  },
+  getRbTwos: () => {
+    // return RB2 avgs (13-24)
+    var result = [];
+    for (var i = runningbacks.length - 13; i >= runningbacks.length - 24; i--){
+      var current = getRb2016.calcAdditionalStats(runningbacks[i]);
+      result.push(current);
+    }
+    return result;
+  },
+  getRbThrees: () => {
+    // return RB3 avgs (25 - 36)
+    var result = [];
+    for (var i = runningbacks.length - 25; i >= runningbacks.length - 36; i--){
+      var current = getRb2016.calcAdditionalStats(runningbacks[i]);
+      result.push(current);
+    }
+    return result;
   }
 };
 
